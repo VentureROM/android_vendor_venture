3077b8c8044b7abd39a55fe817f6c22b245d2a21
Chintan Shah <cgintan1998@gmail.com>
2015-03-03 21:12:42 -0700

    Remove unneeded lunch combos & add flounder
    - flounder is commented out for now. the support is coming
    
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,9 +3,4 @@ add_lunch_combo venture_grouper-userdebug
 add_lunch_combo venture_hammerhead-userdebug
 add_lunch_combo venture_mako-userdebug
 add_lunch_combo venture_shamu-userdebug
-
-#Legacy
-add_lunch_combo venture_m8-userdebug
-add_lunch_combo venture_jflte-userdebug
-add_lunch_combo venture_bacon-userdebug
-add_lunch_combo venture_d2lte-userdebug
+#add_lunch_combo venture_flounder-userdebug
